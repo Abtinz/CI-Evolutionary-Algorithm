c43c128780a3e051ecfe2833cefdb315c148ea0b
abtinz <abtinzand@gmail.com>
2023-06-29 04:59:02 +0330

    score

--- game.py
+++ game.py
@@ -25,7 +25,7 @@ class Game:
 
 
     def population(gens_count, chromosome_count):
-    # chromosomes = [[random.randint(0,2) for i in range(len)] for j in range(200)]
+        # chromosomes = [[random.randint(0,2) for i in range(len)] for j in range(200)]
         chromosome = [[np.random.choice(np.arange(0, 3), p=[0.5, 0.4, 0.1]) for i in range(gens_count)] for j in range(chromosome_count)]
         return chromosome
     
@@ -36,13 +36,13 @@ class Game:
 
         current_level = self.levels[self.current_level_index]
         alive_steps = 0
-        current_score = 0
+        score = 0.0
         mushroom_point = 0
         wining = True
         jump_before_end = False
         not_a_good_jump = 0
         killed_gumpas_count = 0
-        
+
         for i in range(self.current_level_len):
             current_step = current_level[i]
             next_step, second_next_step =  '', ''
@@ -97,7 +97,14 @@ class Game:
                     if (second_next_step == 'G' and actions[i+1] != '0'):
                         killed_gumpas_count += 1
 
-        return steps == self.current_level_len - 1, steps
+        score += 0.1 * alive_steps
+        score += 0.3 * mushroom_point
+        score += 0.1 * killed_gumpas_count
+        score -= 0.1 * not_a_good_jump
+        score += 0.1  if(jump_before_end) else 0 
+        score += 0.4  if(wining) else 0 
+
+        return score
     
 
 g = Game(["____G__L__", "___G_M___L_"])
